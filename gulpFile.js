b3afe8eb8b29d27e7dc72622158b0184838301fb
edwinb24 <edwin_b24@hotmail.com>
2019-04-06 12:01:53 -0500

    Project Setup Complete
    All preprocessors set. Decided to leave Sass out of the loop to try a full CSS implementation.

---
+++ gulpFile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssMin = require('gulp-css');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+
+//compile CSS
+function style() {
+  return gulp.src('./src/css/uncompress/**/*')
+  .pipe(concat('styles.css'))
+  .pipe(cssMin())
+  .pipe(gulp.dest('./src/css'))
+  .pipe(browserSync.stream());
+}
+
+//compile JS
+function script() {
+  return gulp.src([
+          "./src/js/scripts/*.js"
+        ])
+        .pipe(concat('libs.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest("./src/js"));
+}
+
+function watch() {
+  browserSync.init({
+    proxy: "local.scheduling-system.com:80"
+  });
+  gulp.watch('./src/css/uncompress/**/*.css', style);
+  gulp.watch('./src/js/scripts/**/*.js', script);
+  gulp.watch('./src/js/*.js').on('change', browserSync.reload);
+  gulp.watch('./*.php').on('change', browserSync.reload);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.script = script;
+exports.watch = watch;
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Class Scheduling System</title>
+  <meta name="description" content="Class Scheduling System">
+  <meta name="author" content="Edwin Broce">
+
+  <link rel="stylesheet" type="text/css" href="src/css/styles.css">
+  <script type="text/javascript" src="src/js/libs.js"></script>
+</head>
+
+<body>
+HELOOOOOOWWWW!! aaaa bbbbb
+</body>
+</html>
